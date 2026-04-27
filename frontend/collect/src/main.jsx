eb739228d75adf896a6660b7ec95c5bd7d18f1b1
aryaman1656 <aryamanpandey67@gmail.com>
2026-04-27 10:52:22 +0530

    fix: use env variable for Google Client ID

--- frontend/collect/src/main.jsx
+++ frontend/collect/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './styles/index.css'
 
-const GOOGLE_CLIENT_ID = '912110345690-sjb2b0o6qu7j4pc63fi9abtcfca8ugqn.apps.googleusercontent.com'
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '912110345690-sjb2b0o6qu7j4pc63fi9abtcfca8ugqn.apps.googleusercontent.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
